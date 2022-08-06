626d2a4304486a3ca4a1978eed673c864f4852b2
Sam Rice <samsrice@gmail.com>
2022-08-05 22:11:04 -0700

    Initial commit

--- oop.js
+++ oop.js
@@ -3,6 +3,22 @@
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn {
+  constructor(name) {
+    this.name = name;
+    this.color = "silver";
+  }
+
+  say(text) {
+    console.log("*~*" + text + "*~*")
+  }
+}
+
+var jesseTheUnicorn = new Unicorn("Jesse");
+
+console.log(jesseTheUnicorn);
+
+jesseTheUnicorn.say("Hey folks");
 
 
 // Write a class called Vampire
@@ -10,6 +26,29 @@
 // it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
+
+class Vampire {
+  constructor(name, pet) {
+    this.name = name;
+    this.pet = pet || "bat"
+    this.thirsty = true;
+  }
+  drink() {
+    this.thirsty = false
+  }
+}
+
+var samsVampire = new Vampire("Vlad",);
+
+console.log(samsVampire);
+
+samsVampire.drink()
+
+console.log(samsVampire);
+
+var otherVampire = new Vampire("Drake", "goat")
+
+console.log(otherVampire);
 
 
 
@@ -21,6 +60,30 @@
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
 
+class Dragon {
+  constructor(name, rider, color) {
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+    this.isHungry = true;
+  }
+  eat(meals) {
+    if (meals >= 4) {
+      this.isHungry = false;
+    }
+  }
+}
+
+
+var samsDragon = new Dragon("Drago", "Harry Potter", "red");
+
+console.log(samsDragon);
+
+samsDragon.eat(4);
+
+console.log(samsDragon);
+
+
 
 
 //  BIG CHALLNEGE: Write a Hobbit class
@@ -31,3 +94,61 @@
 //  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 //  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
 //  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit {
+  constructor(name, disposition, age) {
+    this.name = name;
+    this.disposition = disposition;
+    this.age = age || 0;
+    this.isAdult = false;
+    this.isOld = false;
+    this.hasRing = false;
+
+      if (this.name === "Frodo" && this.age < 33) {
+        this.hasRing = true;
+    } else if (this.name === "Frodo" && this.age > 32 && this.age < 101) {
+        this.hasRing = true;
+        this.isAdult = true;
+    } else if (this.name === "Frodo" && this.age > 100) {
+        this.hasRing = true;
+        this.isAdult = true;
+        this.isOld = true;
+    } else if (this.age > 32 && this.age < 101) {
+        this.isAdult = true;
+    } else if (this.age > 100) {
+        this.isAdult = true;
+        this.isOld = true;
+      }
+  }
+
+celebrateBirthday() {
+    this.age++;
+        if (this.name === "Frodo" && this.age < 33) {
+        this.hasRing = true;
+    } else if (this.name === "Frodo" && this.age > 32 && this.age < 101) {
+        this.hasRing = true;
+        this.isAdult = true;
+    } else if (this.name === "Frodo" && this.age > 100) {
+        this.hasRing = true;
+        this.isAdult = true;
+        this.isOld = true;
+    } else if (this.age > 32 && this.age < 101) {
+        this.isAdult = true;
+    } else if (this.age > 100) {
+        this.isAdult = true;
+        this.isOld = true;
+      }
+  }
+}
+
+var firstHobbit = new Hobbit("Pippin", "goofy", 32);
+console.log(firstHobbit);
+
+firstHobbit.celebrateBirthday();
+console.log(firstHobbit);
+
+var secondHobbit = new Hobbit("Frodo", "serious", 32);
+console.log(secondHobbit);
+
+secondHobbit.celebrateBirthday();
+console.log(secondHobbit);
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,13 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+
+This was the most fun end-of-lesson challenge yet, for me. It felt the way I'd imagine coding a project would be; googling for new techniques, trying to de-bug by making tweaks and running code over and over again, etc. Very satisfying when you get everything to work. (even if it's not done in the most efficient way)
+
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+
+I feel like I have a good balance of work-to-break time. I'm pretty attuned to when I start to struggle unproductively, I think.
+
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+
+I need to work on getting more sleep. I'm a night-owl, by default, and I ran on about 5 hours of sleep every night this week, which won't be sustainable throughout the course.
